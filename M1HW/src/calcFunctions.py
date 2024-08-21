28d96c8ae509e1e94faee9b2eb51da7dccf41c38
Harley Coughlin <145471617+coughlih3099@users.noreply.github.com>
2024-08-20 23:58:46 -0400

    Yeah it works
    Co-authored-by: joklar93 <joklar93@users.noreply.github.com>

--- M1HW/src/calcFunctions.py
+++ M1HW/src/calcFunctions.py
@@ -1,6 +1,5 @@
 from menus import subMenu
 
-
 def getSubMenuInput() -> bool:
     """Calls and gets input for subMenu, returns a bool"""
     isValid = False
@@ -13,32 +12,83 @@ def getSubMenuInput() -> bool:
             if subInput in (1, 2):
                 isValid = True
         except ValueError:
+            print("Please enter 1 or 2.")
             subMenu()
     if subInput == 1:
         return True
     return False
 
 
+def prettyPrint(sdqp: tuple[str, bool, int | float, int | float, int | float]) -> None:
+    if sdqp[0] == "sum":
+        operation = "+"
+    elif sdqp[0] == "difference":
+        operation = "-"
+    elif sdqp[0] == "quotient":
+        operation = "/"
+    elif sdqp[0] == "product":
+        operation = "*"
+
+    if sdqp[1]:
+        print(f"{sdqp[2]} {operation} {sdqp[3]} = {sdqp[4]:.5}")
+    else:
+        print(f"{sdqp[2]} {operation} {sdqp[3]} = {sdqp[4]:}")
+
+
+
+def validateInput() -> int | float:
+    isValid = False
+    while not isValid:
+        userInput = input("Enter a number: ")
+        try:
+            if "." in userInput:
+                userInput = float(userInput)
+                isValid = True
+            else:
+                userInput = int(userInput)
+                isValid = True
+        except ValueError:
+            print("Enter a valid integer or float\n")
+    return userInput
+
 def add() -> bool:
     """Adds two numbers, calls getSubMenuInput, returns bool"""
     print()
     print(f"{"Add":-^29}")
-    userInput1 = int(input("Enter number 1: "))
-    userInput2 = int(input("Enter number 2: "))
-    product = userInput1 + userInput2
-    print(f"{userInput1} + {userInput2} = {product}")
+    userInput1 = validateInput()
+    userInput2 = validateInput()
+    sum = userInput1 + userInput2
+    prettyPrint(("sum", isinstance(sum, float), userInput1, userInput2, sum))
     return getSubMenuInput()
 
-
+             
 def subtract():
-    print("This is the subtract function")
+    print()
+    print(f"{"Subtract":-^29}")
+    userInput1 = validateInput()
+    userInput2 = validateInput()
+    difference = userInput1 - userInput2
+    prettyPrint(("difference", isinstance(difference, float), userInput1, userInput2, difference))
     return getSubMenuInput()
 
 
 def divide():
-    print("This is the divide function")
+    """Divides two numbers, calls getSubMenuInput, returns bool"""
+    print()
+    print(f"{"Divide":-^29}")
+    userInput1 = validateInput()
+    userInput2 = validateInput()
+    quotient = userInput1 / userInput2
+    prettyPrint(("quotient", isinstance(quotient, float), userInput1, userInput2, quotient))
     return getSubMenuInput()
 
+
 def multiply():
-    print("This is the multiply function")
+    """Mutliplies two numbers, calls getSubMenuInput, returns bool"""
+    print()
+    print(f"{"Multiply":-^29}")
+    userInput1 = validateInput()
+    userInput2 = validateInput()
+    product = userInput1 * userInput2
+    prettyPrint(("product", isinstance(product, float), userInput1, userInput2, product))
     return getSubMenuInput()
