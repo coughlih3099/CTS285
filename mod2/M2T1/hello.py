7edf4cd74bd4f66e36bae452dfcf04ed077e8d61
coughlih3099 <145471617+coughlih3099@users.noreply.github.com>
2024-09-11 16:48:05 +0000

    made main template and added renderer to hello.py

--- mod2/M2T1/hello.py
+++ mod2/M2T1/hello.py
@@ -1,17 +1,15 @@
 # minimal Flask app
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return """
-    <h3>Hello, CTS285!!!</h3>
-    <p>This is a paragraph</p>
-    <a href="action">Click here</a>
-    """
+    name = "Harley"
+    return render_template("main_page.html", name=name)
+
 
 @app.route("/action")
 def action():
