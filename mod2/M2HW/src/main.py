e14be23110181129ec9558fb4788c73de5763185
Harley Coughlin <145471617+coughlih3099@users.noreply.github.com>
2024-09-22 10:06:21 -0400

    M2HW1: The main page has a structure

---
+++ mod2/M2HW/src/main.py
@@ -0,0 +1,20 @@
+from fasthtml.common import *
+
+css = Style(":root { --pico-font-size:90%, --pico-font-family: Pacifico, cursive; }")
+
+
+app = FastHTMLWithLiveReload(hdrs=(picolink, css))
+rt = app.route
+
+
+nav_bar = Nav(Ul(Li(Strong("Navbar"))),
+              Ul(Li(Details(Summary("Modes"),
+                    Ul(Li(A("Answer Checker", href="#")),
+                    Li(A("Databank", href="#")),
+                    Li(A("Electro Flash", href="#")), dir="rtl"), cls="dropdown"))), cls="container")
+
+
+@rt("/", methods=["get"])
+def home():
+    to_be_replaced = Body(Div(P("Here's some Text")), cls="container")
+    return Titled("Dataman", nav_bar, to_be_replaced)
