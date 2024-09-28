95a9b2f3fcc74108d069ad624297e26bdd448937
Harley Coughlin <145471617+coughlih3099@users.noreply.github.com>
2024-09-28 09:47:11 -0400

    MadMath: Rebrand answer checker to math master

--- MadMath/src/main.py
+++ MadMath/src/main.py
@@ -17,27 +17,32 @@ rt = app.route
 nav_bar = Nav(Ul(Li(Strong("MadMath"))),
               Ul(Li(Details(Summary("Modes"),
                             Ul(
-                                Li(A("Answer Checker", hx_target="", hx_get="")),
-                                Li(A("Memory Bank")),
-                                Li(A("Electro Flash"))),
+                                Li(A("Math Master", hx_target="replacable", hx_get="math-master")),
+                                Li(A("Memory Bank", hx_target="replacable", hx_get="memory-bank")),
+                                Li(A("Electro Flash", hx_target="replacable", hx_get="electro-flash"))),
                     cls="dropdown"))),
               cls="container")
 
 
 def get_input(**kw):
-    return Input(id="user_input", name="user_input", placeholder="Enter problem here", **kw)
+    return Input(id="user_input", name="user_input", placeholder="Enter problem here", autocomplete="off", **kw)
 
 
 @rt("/", methods=["get"])
-def answer_checker():
-    head = "Welcome to MadMath's Answer Checker!"
+def index():
+    return Redirect("/math-master")
+
+
+@rt("/math-master", methods=["get"])
+def math_master():
+    head = "Welcome to MadMath's Math Master!"
     instructions = Fieldset(Legend("Instructions:"),
                             Div(P("1. Enter a problem, e.g. 1 + 1 = 2"),
                                 P("2. Press Submit"),
-                                P("3. Answer Checker will tell you if you got the correct answer"),
-                                P("4. Try again or try a new question")), style="border-right: 1px solid white")
-    history = Fieldset(Legend("Problem History:"), Div(id="history"))
+                                P("3. Math Master will tell you if you got the correct answer"),
+                                P("4. Try again or try a new question")))
+    history = Fieldset(Legend("Problem History:"), Div(id="history"), style="padding-left: 10px; border-left: 1px solid white")
     middle = Grid(instructions, history)
     add = Form(Group(get_input(required=True), Button("Submit")))
-    answer_check = Card(middle, header=head, footer=add)
-    return Title("MadMath"), nav_bar, Div(answer_check, id="replacable", cls="container")
+    math_master = Card(middle, header=head, footer=add)
+    return Title("MadMath"), nav_bar, Div(math_master, id="replacable", cls="container")
