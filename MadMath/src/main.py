4f334116e68c3ac2eeb441f0b3960a5b852482cf
Harley Coughlin <145471617+coughlih3099@users.noreply.github.com>
2024-09-29 16:18:23 -0400

    MadMath: reworking main.py

--- MadMath/src/main.py
+++ MadMath/src/main.py
@@ -17,32 +17,41 @@ rt = app.route
 nav_bar = Nav(Ul(Li(Strong("MadMath"))),
               Ul(Li(Details(Summary("Modes"),
                             Ul(
-                                Li(A("Math Master", hx_target="replacable", hx_get="math-master")),
-                                Li(A("Memory Bank", hx_target="replacable", hx_get="memory-bank")),
-                                Li(A("Electro Flash", hx_target="replacable", hx_get="electro-flash"))),
+                                Li(A("Math Master", hx_target="#replacable", hx_get="/math-master")),
+                                Li(A("Memory Bank", hx_target="#replacable", hx_get="/memory-bank")),
+                                Li(A("Electro Flash", hx_target="#replacable", hx_get="/electro-flash"))),
                     cls="dropdown"))),
               cls="container")
+
+
+@rt("/", methods=["get"])
+def index():
+    return Title("MadMath"), nav_bar, Div(id="replacable", cls="container", hx_get="/math-master", hx_trigger="load")
 
 
 def get_input(**kw):
     return Input(id="user_input", name="user_input", placeholder="Enter problem here", autocomplete="off", **kw)
 
 
-@rt("/", methods=["get"])
-def index():
-    return Redirect("/math-master")
-
-
 @rt("/math-master", methods=["get"])
 def math_master():
     head = "Welcome to MadMath's Math Master!"
-    instructions = Fieldset(Legend("Instructions:"),
-                            Div(P("1. Enter a problem, e.g. 1 + 1 = 2"),
-                                P("2. Press Submit"),
-                                P("3. Math Master will tell you if you got the correct answer"),
-                                P("4. Try again or try a new question")))
-    history = Fieldset(Legend("Problem History:"), Div(id="history"), style="padding-left: 10px; border-left: 1px solid white")
-    middle = Grid(instructions, history)
+    instructions = Div(P("1. Enter a problem, e.g. 1 + 1 = 2"),
+                        P("2. Press Submit"),
+                        P("3. Math Master will tell you if you got the correct answer"),
+                        P("4. Try again or try a new question"))
+    history = Div(id="history", style="padding-left: 10px; border-left: 1px solid white")
     add = Form(Group(get_input(required=True), Button("Submit")))
-    math_master = Card(middle, header=head, footer=add)
-    return Title("MadMath"), nav_bar, Div(math_master, id="replacable", cls="container")
+    middle = Grid(Card(instructions, header="Instructions:"), Card(history, header="Problem History:", footer=add))
+    math_master = Card(middle, header=head)
+    return math_master
+
+
+@rt("/memory-bank", methods=["get"])
+def memory_bank():
+    return Card("Memory Bank")
+
+
+@rt("/electro-flash", methods=["get"])
+def electro_flash():
+    return Card("Electro Flash")
