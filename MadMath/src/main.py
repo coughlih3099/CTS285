50ee40689c5b584505d22feb75cc72326b6ede62
Harley Coughlin <145471617+coughlih3099@users.noreply.github.com>
2024-10-16 12:28:00 -0400

    MadMath: Changed initial page back to math-master

--- MadMath/src/main.py
+++ MadMath/src/main.py
@@ -24,8 +24,7 @@ nav_bar = Nav(Ul(Li(Strong("MadMath"))),
 
 @rt("/", methods=["get"])
 def index():
-    # TODO: Swap the hx_get back to /math-master
-    return Title("MadMath"), nav_bar, Div(id="main-card", cls="container", hx_get="/madness-methods", hx_trigger="load")
+    return Title("MadMath"), nav_bar, Div(id="main-card", cls="container", hx_get="/math-master", hx_trigger="load")
 
 def get_input(**kw) -> str:
     return Input(id="user_input", name="user_input", autocomplete="off", required=True, **kw)
