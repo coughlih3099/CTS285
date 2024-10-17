2150745b80491ec3e738df9cbb6b381185366ee7
Harley Coughlin <145471617+coughlih3099@users.noreply.github.com>
2024-10-17 19:20:04 -0400

    MadMath: answer_checker, get_answer implemented

--- MadMath/src/answer_checker.py
+++ MadMath/src/answer_checker.py
@@ -19,10 +19,28 @@ def multiply(num1: int, num2: int) -> int:
     return num1 * num2
 
 
+def get_answer(num1: int, num2: int, operation: str) -> int:
+    match operation:
+        case "+":
+            answer = add(num1, num2)
+        case "-":
+            # there should never be a time where num2 is greater than num1
+            if num2 > num1:
+                raise ValueError
+            answer = subtract(num1, num2)
+        case "/":
+            # there should never be a time where num2 is 0
+            if num2 == 0:
+                raise ZeroDivisionError
+            answer = divide(num1, num2)
+        case "*":
+            answer = multiply(num1, num2)
+    return answer
+
 
 def check_answer(num1: int, num2: int, operation: str, answer: int) -> bool:
     result = None
-    match(operation):
+    match operation:
         case "+":
             result = add(num1, num2)
         case "-":
